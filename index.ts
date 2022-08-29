e5533c22d7bc698f52af01ce0326b227ff26d329
chrisvuong20@gmail.com <chrisvuong20@gmail.com>
2022-08-30 00:54:38 +1000

    add index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import input from './utils/input';
+import cmd from './version/cmd';
+
+async function run() {
+    var command:any = await input("Press 1 for command line interface, 2 to UI and other to quit: ");
+    if(parseInt(command) == 1){
+        console.log("Command line interface has started ... ");
+        cmd();
+    }else if (parseInt(command) == 2){
+        // start local host
+    }
+}
+run();
